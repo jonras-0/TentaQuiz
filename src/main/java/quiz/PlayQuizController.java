f0232bbbe7f059d9e22f38d944c6f14632c3dc91
jonras-0 <jonras-0@student.ltu.se>
2022-05-27 15:11:53 +0200

    Nu går det välja svarsalternativ med siffror. Enter går använda för att gå vidare till nästa fråga även om andra knappar/fält har fokus.

--- src/main/java/quiz/PlayQuizController.java
+++ src/main/java/quiz/PlayQuizController.java
@@ -3,11 +3,15 @@ package quiz;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import model.Alternative;
 import model.SingleChoiceQuestion;
 import model.Question;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class PlayQuizController {
@@ -120,19 +124,35 @@ public class PlayQuizController {
         alternativeBtnList = new ArrayList<>();
         ToggleGroup toggleGroup = new ToggleGroup();
 
+//        for (int i = 1 ; i <= currentAlternatives.length; i++) {
+//            if (currentQuestion instanceof SingleChoiceQuestion) {
+//                RadioButton rB = new RadioButton(i + ". " + currentAlternatives[i].getAlternativeText());
+//                rB.setWrapText(true);
+//                rB.setToggleGroup(toggleGroup);
+//                alternativeBtnList.add(rB);
+//                alternativesVBox.getChildren().add(rB);
+//            } else {
+//                CheckBox cB = new CheckBox(i + ". " + currentAlternatives[i].getAlternativeText());
+//                alternativeBtnList.add(cB);
+//                cB.setWrapText(true);
+//                alternativesVBox.getChildren().add(cB);
+//            }
+//        }
+        int counter = 1;
         for (Alternative a : currentAlternatives) {
             if (currentQuestion instanceof SingleChoiceQuestion) {
-                RadioButton rB = new RadioButton(a.getAlternativeText());
+                RadioButton rB = new RadioButton(counter + ". " + a.getAlternativeText());
                 rB.setWrapText(true);
                 rB.setToggleGroup(toggleGroup);
                 alternativeBtnList.add(rB);
                 alternativesVBox.getChildren().add(rB);
             } else {
-                CheckBox cB = new CheckBox(a.getAlternativeText());
+                CheckBox cB = new CheckBox(counter + ". " + a.getAlternativeText());
                 alternativeBtnList.add(cB);
                 cB.setWrapText(true);
                 alternativesVBox.getChildren().add(cB);
             }
+            counter++;
         }
         alternativesBorderPane.setCenter(alternativesVBox);
     }
@@ -142,5 +162,40 @@ public class PlayQuizController {
             showCorrectAlternativesCheatCheckMenuItem.setSelected(true);
         }
         enableOrDisableAlternativesCheat();
+    }
+
+    @FXML
+    private void btnPressed(KeyEvent keyEvent) {
+        KeyCode keyCode = keyEvent.getCode();
+        if (keyCode.equals(KeyCode.ENTER))
+            answerQuestion();
+        else if (keyCode.isDigitKey()){
+            KeyCode[] digitKeyCodes = {KeyCode.DIGIT1, KeyCode.DIGIT2, KeyCode.DIGIT3,
+                    KeyCode.DIGIT4, KeyCode.DIGIT5, KeyCode.DIGIT6,
+                    KeyCode.DIGIT7, KeyCode.DIGIT8, KeyCode.DIGIT9};
+            for (int i = 0; i < digitKeyCodes.length; i++) {
+                if (keyCode.equals(digitKeyCodes[i])) {
+                    checkOrUncheckAlternative(i);
+                    break;
+                }
+            }
+        }
+    }
+    private void checkOrUncheckAlternative(int btnIndex) {
+        if (alternativeBtnList.size() > btnIndex) {
+            ButtonBase button = alternativeBtnList.get(btnIndex);
+            if (button instanceof RadioButton) {
+                RadioButton radioButton = (RadioButton) button;
+                if (!radioButton.isSelected())
+                    radioButton.setSelected(true);
+            } else if (button instanceof CheckBox) {
+                CheckBox checkBox = (CheckBox) button;
+                if (checkBox.isSelected())
+                    checkBox.setSelected(false);
+                else
+                    checkBox.setSelected(true);
+            }
+        }
+
     }
 }
